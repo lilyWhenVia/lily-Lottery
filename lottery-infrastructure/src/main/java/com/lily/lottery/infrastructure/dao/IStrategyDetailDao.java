a9f91915d0788aeaebd6fbb6f73511b060381e14
lily via <lily228250998@gmail.com>
2024-06-16 17:06:29 +0800

    <新功能>新增数据层查询语句以及dao层方法

--- lottery-infrastructure/src/main/java/com/lily/lottery/infrastructure/dao/IStrategyDetailDao.java
+++ lottery-infrastructure/src/main/java/com/lily/lottery/infrastructure/dao/IStrategyDetailDao.java
@@ -7,11 +7,32 @@ import java.util.List;
 
 /**
  * Created by lily via on 2024/6/14 20:25
+ * 策略细节表数据库操作
  */
 @Mapper
 public interface IStrategyDetailDao {
 
-
+    /**
+     * 根据策略ID查询策略细节表列表
+     * @param strategyId 策略ID
+     * @return           返回结果
+     */
     List<StrategyDetail> queryStrategyDetailList(Long strategyId);
+
+    /**
+     * 查询无库存策略奖品ID
+     * @param strategyId 策略ID
+     * @return           返回结果
+     */
+    List<String> queryNoStockStrategyAwardList(Long strategyId);
+
+    /**
+     * 扣减库存操作
+     * @param strategyDetailReq 策略ID、奖品ID
+     * @return                  返回结果
+     */
+    int deductStock(StrategyDetail strategyDetailReq);
+
+
 
 }
