dc171bc444a70c541607f3e5d76bd59f7f3ba7dd
lily via <lily228250998@gmail.com>
2024-06-16 18:17:58 +0800

    <update>修改奖品类封装信息

--- lottery-domain/src/main/java/com/lily/lottery/domain/strategy/model/res/DrawResult.java
+++ lottery-domain/src/main/java/com/lily/lottery/domain/strategy/model/res/DrawResult.java
@@ -4,6 +4,8 @@ package com.lily.lottery.domain.strategy.model.res;
  * Created by lily via on 2024/6/14 20:50
  */
 
+import com.lily.lottery.common.Constants;
+import com.lily.lottery.domain.strategy.model.vo.DrawAwardInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,9 +24,21 @@ public class DrawResult {
     // 策略ID
     private Long strategyId;
 
-    private String awardId;
+    /**
+     * 中奖状态：0未中奖、1已中奖、2兜底奖 Constants.DrawState
+     */
+    private Integer drawState = Constants.DrawState.FAIL.getCode();
 
-    private String awardName;
+    /**
+     * 中奖奖品信息
+     */
+    private DrawAwardInfo drawAwardInfo;
 
+
+    public DrawResult(String uId, Long strategyId, Integer drawState) {
+        this.uId = uId;
+        this.strategyId = strategyId;
+        this.drawState = drawState;
+    }
 
 }
--- lottery-domain/src/main/java/com/lily/lottery/domain/strategy/model/vo/AwardRateInfo.java
+++ lottery-domain/src/main/java/com/lily/lottery/domain/strategy/model/vo/AwardRateInfo.java
@@ -4,11 +4,18 @@ package com.lily.lottery.domain.strategy.model.vo;
  * Created by lily via on 2024/6/14 20:53
  */
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
 /**
  * 奖品概率信息，奖品ID
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AwardRateInfo {
 
     // 奖品ID
@@ -16,29 +23,5 @@ public class AwardRateInfo {
 
     // 中奖概率
     private BigDecimal awardRate;
-
-    public AwardRateInfo() {
-    }
-
-    public AwardRateInfo(String awardId, BigDecimal awardRate) {
-        this.awardId = awardId;
-        this.awardRate = awardRate;
-    }
-
-    public String getAwardId() {
-        return awardId;
-    }
-
-    public void setAwardId(String awardId) {
-        this.awardId = awardId;
-    }
-
-    public BigDecimal getAwardRate() {
-        return awardRate;
-    }
-
-    public void setAwardRate(BigDecimal awardRate) {
-        this.awardRate = awardRate;
-    }
 
 }
